29bb357182b66c4cc7ebbb20467b52505459649d
Roberto Clavijo <robertoclavijob@gmail.com>
2022-04-25 11:05:54 -0400

    Base project

---
+++ src/main/java/com/robertoclavijob/feignexample/FeignexampleApplication.java
@@ -0,0 +1,14 @@
+package com.robertoclavijob.feignexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignexampleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("");
+		SpringApplication.run(FeignexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robertoclavijob/feignexample/FeignexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertoclavijob.feignexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
