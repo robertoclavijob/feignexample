185b6f2a6427de2d5accbc790de0907787ca1e65
Roberto Clavijo <robertoclavijob@gmail.com>
2022-06-23 16:23:10 -0400

    Refactor code and remove duplicated code

--- src/main/java/com/robertoclavijob/feignexample/controller/TheCatApiCustomController.java
+++ src/main/java/com/robertoclavijob/feignexample/controller/TheCatApiCustomController.java
@@ -3,7 +3,6 @@ package com.robertoclavijob.feignexample.controller;
 import com.robertoclavijob.feignexample.model.GeneralResponse;
 import com.robertoclavijob.feignexample.model.Vote;
 import com.robertoclavijob.feignexample.service.TheCatApiCustomService;
-import com.robertoclavijob.feignexample.service.TheCatApiSpringService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
